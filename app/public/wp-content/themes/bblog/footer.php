02e2ee514c58d2e89d92b09f2ac801ce49db5fac
karmalntest <karmalntest@gmail.com>
2022-08-30 12:10:38 +0530

    first commit

---
+++ app/public/wp-content/themes/bblog/footer.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package AKB_Blog
+ */
+
+?>
+
+<footer id="colophon" class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="site-info">
+                    <a href="#">
+                       Karmaleen
+                    </a>
+                </div><!-- .site-info -->
+            </div>
+        </div>
+    </div>
+
+</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
